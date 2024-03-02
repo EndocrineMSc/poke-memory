436839a8af72e5eae123153e813beb0006ead9c8
endocrine <117483689+EndocrineMSc@users.noreply.github.com>
2024-03-02 14:59:49 +0100

    feat: Finalize CSS Layout and colors

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 /* eslint-disable react/jsx-filename-extension */
 import './App.css';
-import CardField from './components/CardField';
+import GameHeader from './components/GameHeader';
 import Counter from './components/Counter';
+import CardField from './components/CardField';
 
 function App() {
   return (
-    <>
+    <div className="gameBody">
       <Counter />
+      <GameHeader />
       <CardField />
-    </>
+    </div>
   );
 }
 
